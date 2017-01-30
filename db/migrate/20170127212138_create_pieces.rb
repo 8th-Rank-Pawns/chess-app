ce961ded87a2118132f65fc48d76ef7201c49dd8
Rajas Pargaonkar <rajas@waddleworks.com>
2017-01-30 05:21:18 +0000

    moved rubocop to do to rubocop.yml

--- db/migrate/20170127212138_create_pieces.rb
+++ db/migrate/20170127212138_create_pieces.rb
@@ -1,12 +1,12 @@
 class CreatePieces < ActiveRecord::Migration
   def change
     create_table :pieces do |t|
-    	t.integer :piece_type
-    	t.integer :vertical_position
-    	t.integer :horizontal_position
-    	t.integer :user_id
-    	t.integer :game_id
-    	
+      t.integer :piece_type
+      t.integer :vertical_position
+      t.integer :horizontal_position
+      t.integer :user_id
+      t.integer :game_id
+
       t.timestamps
     end
   end
