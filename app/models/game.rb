3da70651b1a30b5ebe34b5887e5e20196a450e53
Rajas Pargaonkar <rajas@waddleworks.com>
2017-01-31 20:46:23 +0000

    playing with scope

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,8 @@
 class Game < ActiveRecord::Base
+  scope :available, -> { where(black_id is null or white_id is null) }
+  #scope :available, -> { where(white_id: nil) }
+
 end
+
+#http://stackoverflow.com/questions/17192829/does-rails-4-have-support-for-or-queries
+#Note, null for sql.
