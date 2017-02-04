1b4296545f0248f495791c8537f941c48ec9b483
Kevin Oglesby <kevinjoglesby@gmail.com>
2017-02-04 23:55:57 +0000

    list available games on homepage

--- spec/controllers/chess_controller_spec.rb
+++ spec/controllers/chess_controller_spec.rb
@@ -7,5 +7,20 @@ RSpec.describe ChessController, type: :controller do
       expect(response).to have_http_status(:success)
     end
 
+    it 'should list games with less than 2 players as available' do
+    	game = FactoryGirl.create(:game)
+      get :index
+      expect(response).to have_http_status(:success)
+      games = Game.available
+      expect(games.count).to eq(1)
+    end
+
+    it 'should not list games with 2 players as available' do
+    	game = FactoryGirl.create(:fullgame)
+      get :index
+      expect(response).to have_http_status(:success)
+      games = Game.available
+      expect(games.count).to eq(0)
+    end  
   end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -7,7 +7,13 @@ FactoryGirl.define do
     password_confirmation 'secretPassword'
   end
 
-  factory :game do
-    name 'Player 1'
-  end
+	factory :game do
+		name 'Player 1'
+	end
+
+  factory :fullgame, class: Game do 
+		name 'full game'
+		white_player { FactoryGirl.create(:user).id }
+		black_player { FactoryGirl.create(:user).id }
+	end
 end
