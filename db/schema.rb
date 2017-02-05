a936330b8b338c5350ab8c58ca700317dbc2a29d
Julius Zerwick <jrzerwick@gmail.com>
2017-02-05 17:28:30 +0000

    Add type to pieces database and setup STI for piece models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170128084839) do
+ActiveRecord::Schema.define(version: 20170205165857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20170128084839) do
     t.integer  "game_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "type"
   end
 
   create_table "users", force: true do |t|
