8dca073474a1ca2d411e25c88307e3cb32ffbf8c
Rajas Pargaonkar <rajas@waddleworks.com>
2017-02-05 05:53:00 +0000

    Game scope added.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,8 +1,4 @@
 class Game < ActiveRecord::Base
-  scope :available, -> { where(black_id is null or white_id is null) }
-  #scope :available, -> { where(white_id: nil) }
+  scope :available, -> { where(black_id :null).or(where(white_id :null)) }
 
 end
-
-#http://stackoverflow.com/questions/17192829/does-rails-4-have-support-for-or-queries
-#Note, null for sql.
