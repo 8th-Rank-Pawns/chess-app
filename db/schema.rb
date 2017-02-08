fff82d6fc3cd3a5772af43286b30920e0df36159
Russell Kranz <russkranz@yahoo.com>
2017-02-08 03:26:15 +0000

    removed color column from users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170206212106) do
+ActiveRecord::Schema.define(version: 20170208032501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,7 +52,6 @@ ActiveRecord::Schema.define(version: 20170206212106) do
     t.string   "uid"
     t.string   "name"
     t.string   "image"
-    t.string   "color"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
