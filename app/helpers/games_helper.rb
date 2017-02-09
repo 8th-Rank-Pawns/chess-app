ae147d5c0bbb67e1c0c64c20ba1ee99eed69fb1a
Kevin Oglesby <kevinjoglesby@gmail.com>
2017-02-09 18:21:50 +0000

    render piece helper method

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,6 @@
 module GamesHelper
+  def render_piece(x, y)
+  	piece = @game.pieces.where(horizontal_position: x, vertical_position: y).first
+    return "#{piece.color} #{piece.type}" if piece
+  end
 end
