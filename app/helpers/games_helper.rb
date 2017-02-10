6c3a6198be65628b628324497eba30d2a91c4324
Kevin Oglesby <kevinjoglesby@gmail.com>
2017-02-10 21:53:52 +0000

    load piece images on board

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,6 +1,7 @@
 module GamesHelper
   def render_piece(x, y)
     piece = @game.pieces.where(horizontal_position: x, vertical_position: y).first
-    return "#{piece.color} #{piece.type}" if piece
+    piece_name = "#{piece.color}#{piece.type}.png" if piece
+    image_tag(piece_name, class: 'piece-image')
   end
 end
