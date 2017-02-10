87b494d792d53ef2462492f5b88f79889dbf1971
Russell Kranz <russkranz@yahoo.com>
2017-02-10 22:57:56 +0000

    Made start and end values in obstructed method instance variable, and added self to starting x & y positions

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,65 +1,62 @@
 class Piece < ActiveRecord::Base
   belongs_to :user
   belongs_to :game
-<<<<<<< HEAD
-# Every time one of these pieces is moved the "valid_move?" method should be called and that should call the "is_obstructed?" method.
-# game.pieces.where(horizontal_position & vertical_position) are extracted current location of pieces on game board.
+# Every time a piece is moved the "valid_move?" method is called which should call the "is_obstructed?" method and pass it x_end & y_end.
 # If is_obstructed returns false, move is legal.
-# x_end & y_end values are filled when piece is moved.
 # Will write tests later after moving pieces action is more clear...
-  def horizontal_check(x_end, y_end)
-    if x_end < x_start
-      (x_end + 1..x_start - 1).each do |x_between|
+  def horizontal_check
+    if @x_end < @x_start
+      (@x_end + 1..@x_start - 1).each do |x_between|
         return true if game.pieces.where(horizontal_position: x_between).present?
       end
     else
-      (x_start + 1..x_end - 1).each do |x_between|
+      (@x_start + 1..@x_end - 1).each do |x_between|
         return true if game.pieces.where(horizontal_position: x_between).present?
       end
     end
     false
   end
 
-  def vertical_check(x_end, y_end)
-    if y_end < y_start
-      (y_end + 1..y_start - 1).each do |y_between|
+  def vertical_check(@x_end, @y_end)
+    if @y_end < @y_start
+      (@y_end + 1..@y_start - 1).each do |y_between|
         return true if game.pieces.where(vertical_position: y_between).present?
       end
     else
-      (y_start + 1..y_end - 1).each do |y_between|
+      (@y_start + 1..@y_end - 1).each do |y_between|
         return true if game.pieces.where(vertical_position: y_between).present?
       end
     end
     false
   end
 
-  def diagonal_check(x_end, y_end)
-    x_left = x_start < x_end ? x_start : x_end
-    x_right = x_start < x_end ? x_end : x_start
+  def diagonal_check
+    x_left = @x_start < @x_end ? @x_start : @x_end
+    x_right = @x_start < @x_end ? @x_end : x_start
     count = 1
     while count < x_right - x_left
-      if y_end > y_start
-        return true if game.pieces.where(horizontal_position: x_left + count, vertical_position: y_start + count).present?
+      if @y_end > @y_start
+        return true if game.pieces.where(horizontal_position: x_left + count, vertical_position: @y_start + count).present?
       else
-        return true if game.pieces.where(horizontal_position: x_left + count, vertical_position: y_start - count).present?
+        return true if game.pieces.where(horizontal_position: x_left + count, vertical_position: @y_start - count).present?
       end
       count += 1
     end
   end
 
   def obstructed?(x_end, y_end)
-    x_start = Piece.horizontal_position
-    y_start = Piece.vertical_position
-    if x_end == x_start
-      vertical_check(x_end, y_end)
-    elsif y_end == y_start
-      horizontal_check(x_end, y_end)
-    elsif ((y_end - y_start) / (x_end - x_start)).abs == 1
-      diagonal_check(x_end, y_end)
+    @x_start = self.horizontal_position
+    @y_start = self.vertical_position
+    @x_end = x_end
+    @y_end = y_end
+    if x_end == @x_start
+      vertical_check
+    elsif y_end == @y_start
+      horizontal_check
+    elsif ((y_end - @y_start) / (x_end - @x_start)).abs == 1
+      diagonal_check
     else
       false
     end
   end
-=======
->>>>>>> 3838e6da83d2d6277c08ad873c6e8fb342f3869d
 end
