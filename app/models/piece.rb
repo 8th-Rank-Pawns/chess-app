96d7edb2f01de2a94cf30343d794e65eeb50e72a
Russell Kranz <russkranz@yahoo.com>
2017-02-11 02:20:23 +0000

    removed self, fixed diagonal check complexity by creating seperate method, got rid of nested if statement in diagonal check

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,9 +1,9 @@
 class Piece < ActiveRecord::Base
   belongs_to :user
   belongs_to :game
-# Every time a piece is moved the "valid_move?" method is called which should call the "is_obstructed?" method and pass it x_end & y_end.
-# If is_obstructed returns false, move is legal.
-# Will write tests later after moving pieces action is more clear...
+  # Every time a piece is moved the "valid_move?" method is called which should call the "is_obstructed?" method and pass it x_end & y_end.
+  # If is_obstructed returns false, move is legal.
+  # Will write tests after moving pieces action is more clear.
   def horizontal_check
     if @x_end < @x_start
       (@x_end + 1..@x_start - 1).each do |x_between|
@@ -31,22 +31,19 @@ class Piece < ActiveRecord::Base
   end
 
   def diagonal_check
-    x_left = @x_start < @x_end ? @x_start : @x_end
-    x_right = @x_start < @x_end ? @x_end : x_start
+    find_x_right_and_x_left
     count = 1
     while count < x_right - x_left
-      if @y_end > @y_start
-        return true if game.pieces.where(horizontal_position: x_left + count, vertical_position: @y_start + count).present?
-      else
-        return true if game.pieces.where(horizontal_position: x_left + count, vertical_position: @y_start - count).present?
-      end
+      return true if @y_end > @y_start && game.pieces.where(horizontal_position: @x_left + count, vertical_position: @y_start + count).present?
+      return true if @y_end > @y_start && game.pieces.where(horizontal_position: @x_left + count, vertical_position: @y_start - count).present?
       count += 1
     end
+    false
   end
 
   def obstructed?(x_end, y_end)
-    @x_start = self.horizontal_position
-    @y_start = self.vertical_position
+    @x_start = horizontal_position
+    @y_start = vertical_position
     @x_end = x_end
     @y_end = y_end
     if x_end == @x_start
@@ -58,5 +55,12 @@ class Piece < ActiveRecord::Base
     else
       false
     end
+  end
+
+private
+
+  def find_x_right_and_x_left
+      @x_left = @x_start < @x_end ? @x_start : @x_end
+      @x_right = @x_start < @x_end ? @x_end : @x_start
   end
 end
