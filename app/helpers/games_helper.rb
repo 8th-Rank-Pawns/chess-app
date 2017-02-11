a83ef270673422d5ee3079f4b7461076dade885f
Kevin Oglesby <kevinjoglesby@gmail.com>
2017-02-11 01:57:43 +0000

    fix square padding issue

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -4,4 +4,8 @@ module GamesHelper
     piece_name = "#{piece.color}#{piece.type}.png" if piece
     image_tag(piece_name, class: 'piece-image')
   end
+
+  def change_padding(x, y)
+    return "change-padding" if @game.pieces.where(horizontal_position: x, vertical_position: y).first
+  end
 end
