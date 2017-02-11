36a4595b55fb5436be5711c9f2297735a6449cd4
Lesley Park <lesparkley@gmail.com>
2017-02-11 23:40:00 +0000

    Made requested edits + refactored king.rb

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,16 +1,11 @@
 class King < Piece
   def valid_move? (x_end, y_end)
-    piece = Piece.find(self.id)
-    x_start = piece.x_coord
-    y_start =  piece.y_coord
-    if x_end == x_start && y_end == y_start
-        return false
-    elsif x_end == x_start || x_end == x_start-1 || x_end == x_start+1
-      if y_end == y_start || y_end == y_start-1 || y_end == y_start+1
-        return true
-      end
-    else
+    x_start = horizontal_position
+    y_start = vertical_position
+    if (x_end == x_start && y_end == y_start) || (x_end - x_start).abs > 1) || (y_end - y_start).abs > 1)
       return false
+    else
+      return true
     end
   end
 end
