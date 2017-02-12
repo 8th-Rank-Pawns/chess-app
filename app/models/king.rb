e4e8c4e89e30cbc8a855969cd8588818f6b4bf0e
Lesley Park <lesparkley@gmail.com>
2017-02-12 18:41:49 +0000

    Accidentally deleted a line in the process of previous update

--- app/models/king.rb
+++ app/models/king.rb
@@ -3,6 +3,7 @@ class King < Piece
     if (x_end == horizontal_position && y_end == vertical_position) || (x_end - horizontal_position).abs > 1 || (y_end - horizontal_position).abs > 1
       return false
     else
+      return true
     end
   end
 end
