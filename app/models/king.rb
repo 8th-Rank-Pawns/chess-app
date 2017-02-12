02aecad005825fe073e7e7c4ef8c476da22ca79c
Lesley Park <lesparkley@gmail.com>
2017-02-12 18:38:34 +0000

    Deleted superfluous parenthesis from if statements

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,9 +1,8 @@
 class King < Piece
   def valid_move? (x_end, y_end)
-    if (x_end == horizontal_position && y_end == vertical_position) || (x_end - horizontal_position).abs > 1) || (y_end - horizontal_position).abs > 1)
+    if (x_end == horizontal_position && y_end == vertical_position) || (x_end - horizontal_position).abs > 1 || (y_end - horizontal_position).abs > 1
       return false
     else
-      return true
     end
   end
 end
