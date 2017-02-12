7fd73737d16ef87803a87694879e2e7950bd77b4
Lesley Park <lesparkley@gmail.com>
2017-02-12 19:11:17 +0000

    Trying to appease rubocop

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,9 +1,5 @@
 class King < Piece
-  def valid_move? (x_end, y_end)
-    if (x_end == horizontal_position && y_end == vertical_position) || (x_end - horizontal_position).abs > 1 || (y_end - horizontal_position).abs > 1
-      return false
-    else
-      return true
-    end
+  def valid_move?(x_end, y_end)
+    return true unless (x_end == horizontal_position && y_end == vertical_position) || (x_end - horizontal_position).abs > 1 || (y_end - horizontal_position).abs > 1
   end
 end
