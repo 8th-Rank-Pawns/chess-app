e78aa749dfa66b6d05d2b7a7621cfeadb618c5a4
Russell Kranz <russkranz@yahoo.com>
2017-02-15 07:37:02 +0000

    fixed strong params in pieces controller to account for sti

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,5 +1,4 @@
 class PiecesController < ApplicationController
-
   def show
     @piece = Piece.find(params[:id])
     @game = @piece.game
@@ -14,6 +13,19 @@ class PiecesController < ApplicationController
   private
 
   def piece_params
+    params[:piece] = if params.key? :king
+                       params.delete :king
+                     elsif params.key? :queen
+                       params.delete :queen
+                     elsif params.key? :rook
+                       params.delete :rook
+                     elsif params.key? :knight
+                       params.delete :knight
+                     elsif params.key? :bishop
+                       params.delete :bishop
+                     else
+                       params.delete :pawn
+                     end
     params.require(:piece).permit(:horizontal_position, :vertical_position)
   end
 end
