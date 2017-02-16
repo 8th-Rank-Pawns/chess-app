70b502e3ec6c89260affcaeffb9cc57a62f5b189
Kevin Oglesby <kevinjoglesby@gmail.com>
2017-02-16 23:12:32 +0000

    pawn valid move and rspec tests

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,18 +1,22 @@
 class Pawn < Piece
 	def valid_move?(x_end, y_end)
-		diff_x = horizontal_position - x_end
+		diff_x = (horizontal_position - x_end).abs
 		diff_y = y_end - vertical_position if color == 'white'
 		diff_y = vertical_position - y_end if color == 'black'
 		
-		return false if obstructed?(x_end, y_end) || did_not_move?(x_end, y_end)
-		return true if diff_y == 1 && diff_x == 0 || diff_y == 2 && first_move? && diff_x == 0 || diagonal_capture?(x_end, y_end)
+		return false if obstructed?(x_end, y_end) || did_not_move?(x_end, y_end) || piece_in_front?(x_end, y_end, diff_x, diff_y)
+		return true if diff_y == 1 && diff_x == 0 || diff_y == 2 && first_move? && diff_x == 0 || diagonal_capture?(x_end, y_end, diff_x, diff_y)
 		false
 	end
 
 	private
 
-	def diagonal_capture?(x_end, y_end)
-    
+	def piece_in_front?(x_end, y_end, diff_x, diff_y)
+		return true if diff_y == 1 && diff_x == 0 && game.pieces.where(horizontal_position: x_end, vertical_position: y_end).present?
+  end
+
+	def diagonal_capture?(x_end, y_end, diff_x, diff_y)
+    return true if color == 'white' && diff_x == 1 && diff_y == 1 && game.pieces.where(horizontal_position: x_end, vertical_position: y_end, color: 'black').present? || color == 'black' && diff_x == 1 && diff_y == 1 && game.pieces.where(horizontal_position: x_end, vertical_position: y_end, color: 'white').present?
 	end
 
 	def first_move?
