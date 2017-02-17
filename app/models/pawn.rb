72aab8e63c2857ebbbac2e81dd1358039219ae7f
Kevin Oglesby <kevinjoglesby@gmail.com>
2017-02-17 01:39:44 +0000

    pawn valid move rubocop 3 complexity

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -4,8 +4,11 @@ class Pawn < Piece
     diff_y = y_end - vertical_position if color == 'white'
     diff_y = vertical_position - y_end if color == 'black'
 
-    return false if obstructed?(x_end, y_end) || did_not_move?(x_end, y_end) || piece_in_front?(x_end, y_end, diff_x, diff_y)
-    return true if diff_y == 1 && diff_x.zero? || diff_y == 2 && first_move? && diff_x.zero? || diagonal_capture?(x_end, y_end, diff_x, diff_y)
+    return false if obstructed?(x_end, y_end) || did_not_move?(x_end, y_end)
+    return false if piece_in_front?(x_end, y_end, diff_x, diff_y)
+    return true if diff_y == 1 && diff_x.zero?
+    return true if diff_y == 2 && first_move? && diff_x.zero?
+    return true if diagonal_capture?(x_end, y_end, diff_x, diff_y)
     false
   end
 
@@ -16,8 +19,12 @@ class Pawn < Piece
   end
 
   def diagonal_capture?(x_end, y_end, diff_x, diff_y)
-    return true if color == 'white' && diff_x == 1 && diff_y == 1 && game.pieces.where(horizontal_position: x_end, vertical_position: y_end, color: 'black').present?
-    return true if color == 'black' && diff_x == 1 && diff_y == 1 && game.pieces.where(horizontal_position: x_end, vertical_position: y_end, color: 'white').present?
+    return true if color == 'white' && diagonal_move(diff_x, diff_y) && game.pieces.where(horizontal_position: x_end, vertical_position: y_end, color: 'black').present?
+    return true if color == 'black' && diagonal_move(diff_x, diff_y) && game.pieces.where(horizontal_position: x_end, vertical_position: y_end, color: 'white').present?
+  end
+
+  def diagonal_move(diff_x, diff_y)
+    return true if diff_x == 1 && diff_y == 1
   end
 
   def first_move?
