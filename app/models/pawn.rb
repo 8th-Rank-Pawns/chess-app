25e3688319652bc3d7dfc2a8476ce7caf9634944
Kevin Oglesby <kevinjoglesby@gmail.com>
2017-02-17 21:11:36 +0000

    pawn valid move refactored

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -12,44 +12,32 @@ class Pawn < Piece
   private
 
   def vertical_or_diagonal(x_end, y_end, diff_x, diff_y)
-    return true if vertical_move(x_end, y_end, diff_x, diff_y) || diagonal_capture?(x_end, y_end, diff_x, diff_y)
+    vertical_move(x_end, y_end, diff_x, diff_y) || diagonal_move(x_end, y_end, diff_x, diff_y)
   end
 
   def vertical_move(x_end, y_end, diff_x, diff_y)
-    return false if piece_in_front?(x_end, y_end, diff_x, diff_y)
-    return true if move_one(diff_x, diff_y) || move_two(diff_x, diff_y)
+    !piece_in_front?(x_end, y_end, diff_x, diff_y) && move_one_or_two(diff_x, diff_y)
   end
 
-  def diagonal_capture?(x_end, y_end, diff_x, diff_y)
-    return true if color == 'white' && diagonal_white(x_end, y_end, diff_x, diff_y)
-    return true if color == 'black' && diagonal_black(x_end, y_end, diff_x, diff_y)
+  def diagonal_move(x_end, y_end, diff_x, diff_y)
+    diff_x == 1 && diff_y == 1 && diagonal_capture?(x_end, y_end)
+  end
+
+  def diagonal_capture?(x_end, y_end)
+    return true if color == 'white' && game.pieces.where(horizontal_position: x_end, vertical_position: y_end, color: 'black').present?
+    return true if color == 'black' && game.pieces.where(horizontal_position: x_end, vertical_position: y_end, color: 'white').present?
   end
 
   def piece_in_front?(x_end, y_end, diff_x, diff_y)
-    return true if diff_y == 1 && diff_x.zero? && game.pieces.where(horizontal_position: x_end, vertical_position: y_end).present?
+    diff_y == 1 && diff_x.zero? && game.pieces.where(horizontal_position: x_end, vertical_position: y_end).present?
   end
 
-  def diagonal_white(x_end, y_end, diff_x, diff_y)
-    return true if diagonal_move(diff_x, diff_y) && game.pieces.where(horizontal_position: x_end, vertical_position: y_end, color: 'black').present?
-  end
-
-  def diagonal_black(x_end, y_end, diff_x, diff_y)
-    return true if diagonal_move(diff_x, diff_y) && game.pieces.where(horizontal_position: x_end, vertical_position: y_end, color: 'white').present?
-  end
-
-  def diagonal_move(diff_x, diff_y)
-    return true if diff_x == 1 && diff_y == 1
-  end
-
-  def move_one(diff_x, diff_y)
-    return true if diff_y == 1 && diff_x.zero?
-  end
-
-  def move_two(diff_x, diff_y)
-    return true if diff_y == 2 && diff_x.zero? && first_move?
+  def move_one_or_two(diff_x, diff_y)
+    return false if diff_y == 2 && !first_move?
+    return true if (diff_y == 1 || diff_y == 2) && diff_x.zero?
   end
 
   def first_move?
-    return true if (vertical_position == 2 && color == 'white') || (vertical_position == 7 && color == 'black')
+    (vertical_position == 2 && color == 'white') || (vertical_position == 7 && color == 'black')
   end
 end
