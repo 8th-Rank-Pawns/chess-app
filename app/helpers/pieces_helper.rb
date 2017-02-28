d7da923d2492fc27093ef4a94cd2e49571f0e587
Kevin Oglesby <kevinjoglesby@gmail.com>
2017-02-28 00:12:38 +0000

    pop up modal triggers

--- app/helpers/pieces_helper.rb
+++ app/helpers/pieces_helper.rb
@@ -1,2 +1,6 @@
 module PiecesHelper
+  
+  def pawn_promo
+    return "myModal" if @game.pieces.where(type: 'Pawn', vertical_position: 8).first.present? || @game.pieces.where(type: 'Pawn', vertical_position: 1).first.present? 
+  end
 end
