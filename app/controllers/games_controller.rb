e2fef6220c5949a83fb85d7da8063681acb3671f
Russell Kranz <russkranz@yahoo.com>
2017-03-07 02:04:49 +0000

    assigned user to white player when creating game. Made improvements to UI.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,7 +6,7 @@ class GamesController < ApplicationController
   end
 
   def create
-    @game = Game.create(game_params)
+    @game = Game.create(game_params.merge(white_player: current_user.id))
     @game.populate_board!
     redirect_to game_path(@game)
   end
@@ -18,7 +18,7 @@ class GamesController < ApplicationController
 
   def update
     @game = Game.find(params[:id])
-    if @game.black_player != current_user
+    if @game.black_player == nil
       @game.update_attributes(black_player: current_user.id)
     end
     redirect_to game_path(@game)
@@ -27,6 +27,6 @@ class GamesController < ApplicationController
   private
 
   def game_params
-    params.require(:game).permit(:name, :black_player, :white_player)
+    params.require(:game).permit(:name)
   end
 end
--- spec/controllers/chess_controller_spec.rb
+++ spec/controllers/chess_controller_spec.rb
@@ -9,18 +9,20 @@ RSpec.describe ChessController, type: :controller do
 
     it 'should list games with less than 2 players as available' do
       FactoryGirl.create(:game)
+      FactoryGirl.create(:game)
+      FactoryGirl.create(:game)
+      get :index
+      expect(response).to have_http_status(:success)
+      games = Game.available
+      expect(games.count).to eq(3)
+    end
+
+    it 'should not list games with 2 players as available' do
+      FactoryGirl.create(:game)
       get :index
       expect(response).to have_http_status(:success)
       games = Game.available
       expect(games.count).to eq(1)
-    end
-
-    it 'should not list games with 2 players as available' do
-      FactoryGirl.create(:fullgame)
-      get :index
-      expect(response).to have_http_status(:success)
-      games = Game.available
-      expect(games.count).to eq(0)
     end
   end
 end
